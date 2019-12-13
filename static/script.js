e6a55287b6abcb312323bdeaaa1adcacf3341cb8
NileshPant1999 <nileshpant6969@gmail.com>
2019-12-13 17:45:05 +0530

    add some code for blackjack game

--- static/script.js
+++ static/script.js
@@ -152,9 +152,41 @@ function randomColors (){
 }
     
 // challange 5: blackjack
+let blackjackGame={
+    'you': {'scoreSpan': '#your-blackjack-result','div': '#your-box','score':0},
+    'dealer': {'scoreSpan': '#dealer-blackjack-result','div': "#dealer-box",'score':0},
+}
+const YOU = blackjackGame['you']
+const DEALER = blackjackGame['dealer']
+
+const hitSound = new Audio('static/sounds/swish.m4a');
 
 document.querySelector("#blackjack-hit-button").addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
 function blackjackHit(){
-    alert("ouch you click me");
+    showCard(YOU);
+    showCard(DEALER);
+}
+
+function showCard(activePlayer){
+    let cardImage = document.createElement('img');
+    cardImage.src = 'static/images/Q.png';
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+
+}
+
+function blackjackDeal(){
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    
+    for(i=0; i<yourrImages.length; i++){
+        yourImages[i].remove();
+    }
+
+    for(i=0; i<dealerImages.length; i++){
+        dealerImages[i].remove();
+    }
+
+    
 }
